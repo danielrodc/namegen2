35ace5ac412480fae2e033e4c05fb5c823861360
Daniel Rodriguez <127802925+danielrodc@users.noreply.github.com>
2023-10-30 21:26:39 +0000

    daniel

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let pronoun = ["her", "his", "their", "our", "my"];
+let adj = ["pretty", "hairy", "small", "great", "bad"];
+let noun = ["book", "leg", "table", "box", "cat", "dog"];
+let dom = [".com", ".org", ".co", ".gov", ".edu"];
+
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      for (let m = 0; m < dom.length; m++) {
+        console.log(`${pronoun[i]}${adj[j]}${noun[k]}${dom[m]}`);
+      }
+    }
+  }
+}
